dcd0fd6fce3414f6d9076c7fd1b557212d5cdc3e
Oleg <oastan@ya.ru>
2024-07-03 00:52:04 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,31 @@ public class Main {
         System.out.println("Задача 1");
         int age = 16;
         if (age >= 18) {
-            System.out.println("Ты совершеннолетний");
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " то он не совершенно летний, нужно немного подождать");
         }
-
-        if (age < 18) {
-            System.out.println("Ты не совершеннолетний");
-        }
-        System.out.println("Если возраст  человека равен " + age + ",то он не достиг совершеннолетияя. нежно немного подождать");
 
         System.out.println("Задача 2");
 
         int temp = 11;
-        if (temp < 5) {
-            System.out.println("Нужно надеть шапку");
-        }
-
         if (temp > 5) {
-            System.out.println("На улице " + temp + " градусов, можно не надевать шапку");
+            System.out.println("На улице " + temp + " градусов, шапку нужно надеть");
         }
-
         int temp2 = 3;
         if (temp2 < 5) {
-            System.out.println("На улице " + temp2 + " градуса, шапку нужно надеть");
-        }
-
-        if (temp2 > 5) {
-            System.out.println("Можно не надевать шапку");
+            System.out.println("На улице " + temp2 + " градуса, шапку можно не надевать");
         }
 
         System.out.println("Задача 3");
 
         int speed = 120;
         if (speed > 60) {
-            System.out.println("Скорость превышена");
+            System.out.println("Скорость превышена, нужно заплатить штраф");
         } else {
-            System.out.println("Превышения скорости нет");
+            System.out.println("Можно ездить спокойно");
         }
-        System.out.println("Если скорость " + speed + " км/ч" + " то, придется заплатить штраф");
+
 
         System.out.println("Задача 4");
 
@@ -57,11 +45,11 @@ public class Main {
         System.out.println("Задача 5");
 
         int childage = 13;
-        if (age < 5){
+        if (childage < 5) {
             System.out.println("Если возраст ребенка равен " + childage + " то ему нельзя кататься на аттракционе");
         } else if (childage > 5 && childage < 14) {
             System.out.println("Если возраст ребенка равен " + childage + " то можно кататься в сопровождении взрослого");
-        } else if (childage > 14){
+        } else if (childage > 14) {
             System.out.println("Если возраст ребенка равен " + childage + " то может кататься без сопровождения взрослого");
         }
 
@@ -72,8 +60,10 @@ public class Main {
             System.out.println("Есть сидячие места в вагоне");
         } else if (seating == 60 && capacityVan < 102) {
             System.out.println("Есть в вагоне стоячие места");
-        } else
+        } else if (seating == 60 && capacityVan >= 102) ;
+        {
             System.out.println("Вагон полностью забит");
+        }
 
         System.out.println("Задача 7");
         int one = 1;
